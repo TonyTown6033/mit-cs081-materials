fcdfe34a2c9e73d75f347545534708ae0c9ac3bf
tonytown <tonytown6033@gmail.com>
2026-04-10 15:34:18 +0800

    Add sleep user command

---
+++ source/xv6-riscv/user/sleep.c
@@ -0,0 +1,21 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+  if(argc < 2){
+    fprintf(2, "Usage: sleep seconds ...\n");
+    exit(1);
+  }
+
+  int seconds = atoi(argv[1]);
+  if(seconds <= 0){
+    fprintf(2, "sleep: invalid seconds %d\n", seconds);
+    exit(1);
+  }
+  fprintf(1, "(nothing happens for a little while)\n");
+  pause(seconds * 10);
+
+  exit(0);
+}
